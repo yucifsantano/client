4699db164765b1ba0b8096c202c9ee6b71967b61
Yucif Santano <ysantano@ciericorp.com>
2022-02-11 14:23:05 -0600

    Primeras pruebas login

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
 import { useLogging } from './src/hooks/useLogging';
+import * as Svg from 'react-native-svg';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const  { width, height } = Dimensions.get('window');
 
 export default function App() {
   const [logging] = useLogging('Application');
@@ -12,8 +16,21 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={styles.titulo}>Hola</Text>
+      <Text style={styles.subTitle}>Inicie sesión en su cuenta</Text>
+      <TextInput
+        placeholder='minombre@correo.com'
+        style={styles.textInput}
+      >
+
+      </TextInput>
+      <TextInput
+        placeholder='clave de acceso'
+        style={styles.textInput}
+      >
+
+      </TextInput>
     </View>
   );
 }
@@ -21,8 +38,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'teal',
+    backgroundColor: '#ffe2b5',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize:80,
+    color:'#000',
+    fontWeight:'bold'
+  },
+  subTitle: {
+    fontSize:20,
+    color:'gray',
+  },
+  textInput: {
+    padding:10,
+    paddingStart:30,
+    width:'80%',
+    height:50,
+    marginTop:20,
+    borderRadius: 15,
+    backgroundColor: '#fff',
+  }
 });
