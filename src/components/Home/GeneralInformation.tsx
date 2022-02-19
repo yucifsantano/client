b90ca1f04bafb09669c6cbcd11451b1eb86e5964
Yucif Santano <ysantano@ciericorp.com>
2022-02-19 13:13:02 -0600

    202202191312

--- src/components/Home/GeneralInformation.tsx
+++ src/components/Home/GeneralInformation.tsx
@@ -1,23 +1,39 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Dimensions } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { COLORS, FONTS, SIZES } from '../../../constants'
-
-
 
 const GeneralInformation = (props:any) => {
     const { datos } = props;
-    const { id, name, username, phone, email, website } = datos;
+    const { id, name, username, phone, email, website, address } = datos;
+    
+
+    const geo = () => {
+        console.log('lat: ' + address.geo.lat);
+        console.log('lng: ' + address.geo.lng);
+    }
 
     return (
-        <View style={styles.container}>
-            <Text>{id}</Text>
-            <Text>{name}</Text>
-            <Text>{username}</Text>
-            <Text>{phone}</Text>
-            <Text>{email}</Text>
-            <Text>{website}</Text>
-        </View>
+        <TouchableOpacity style={styles.container} onPress={() => (geo())}>
+            <Text style={[FONTS.textHeader, {textAlign:'center', fontSize: SIZES.text17, color: COLORS.colorAlert}]}>{id}</Text>
+            <Text style={[FONTS.textHeader, {textAlign:'center', marginBottom: 15}]}>{name}</Text>
+
+            <View style={{flexDirection: 'row'}}>
+                <Text style={styles.label}>Nombre:</Text>
+                <Text style={{flex:1, alignItems:'flex-start', fontSize: SIZES.text16,}}>{username.toUpperCase()}</Text>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+                <Text style={styles.label}>Teléfono:</Text>
+                <Text style={{flex:1, alignItems:'flex-start', fontSize: SIZES.text16,}}>{phone}</Text>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+                <Text style={styles.label}>Email:</Text>
+                <Text style={{flex:1, alignItems:'flex-start', fontSize: SIZES.text16,}}>{email.toLowerCase()}</Text>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+                <Text style={styles.label}>WEB:</Text>
+                <Text style={{flex:1, alignItems:'flex-start', fontSize: SIZES.text16,}}>{website}</Text>
+            </View>
+        </TouchableOpacity>
     )
 }
 
@@ -25,16 +41,22 @@ const styles = StyleSheet.create({
     container: {
         top: 15,
         width: SIZES.width - 50,
-        height: 250,
+        height: 200,
         padding: 10,
         borderWidth: 0.75,
         borderRadius: SIZES.radius,
         borderColor: COLORS.colorSubTitle,
-
-
+        borderBottomWidth: 7.5,
         backgroundColor: COLORS.colorBackgroundModal,
         ...FONTS.shadowObj
     },
+    label: {
+        flex:0.25, 
+        alignItems:'flex-end', 
+        textAlign:'right', 
+        paddingRight: 10,
+        color: COLORS.colorLabelText,
+    }
 })
 
 export default GeneralInformation;
